1685b2c58bfbe8c74695b14dd993cf3d853714c4
Yago Fernandes <yagoalmeida@gmail.com>
2023-10-13 15:46:03 -0300

    Refactors Modelling/Maths to TS

---
+++ packages/modeling/src/maths/types.ts
@@ -0,0 +1,15 @@
+import type {Vec1} from "./vec1/type.js";
+import type {Vec2} from "./vec2/type.js";
+import type {Vec3} from "./vec3/type.js";
+import type {Vec4} from "./vec4/type.js";
+
+export type {Line2} from "./line2/type.js";
+export type {Line3} from "./line3/type.js";
+export type {Mat4} from "./mat4/type.js";
+export type {Plane} from "./plane/type.js";
+export type {Vec1} from "./vec1/type.js";
+export type {Vec2} from "./vec2/type.js";
+export type {Vec3} from "./vec3/type.js";
+export type {Vec4} from "./vec4/type.js";
+
+export type Vec = Vec1 | Vec2 | Vec3 | Vec4;
