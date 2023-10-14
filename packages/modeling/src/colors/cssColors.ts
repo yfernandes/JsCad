4aa0129a89ec7daae13df4cdf1eb7ee50bbd92be
Yago Fernandes <yagoalmeida@gmail.com>
2023-10-14 19:32:21 -0300

    Change All to .ts

--- packages/modeling/src/colors/cssColors.ts
+++ packages/modeling/src/colors/cssColors.ts
@@ -2,7 +2,9 @@ import type {RGB} from "./types.d.ts";
 type CSSColors = {
 	[color: string]: RGB;
 };
-
+function createRGB(r: number, g: number, b: number): RGB {
+	return [r / 255, g / 255, b / 255];
+}
 /**
  * @alias module:modeling/colors.cssColors
  * @see CSS color table from http://www.w3.org/TR/css3-color/
@@ -12,316 +14,153 @@ type CSSColors = {
  */
 export const cssColors: CSSColors = {
 	// basic color keywords
-	black: [0 / 255, 0 / 255, 0 / 255],
-	silver: [192 / 255, 192 / 255, 192 / 255],
-	gray: [128 / 255, 128 / 255, 128 / 255],
-	white: [255 / 255, 255 / 255, 255 / 255],
-	maroon: [128 / 255, 0 / 255, 0 / 255],
-	red: [255 / 255, 0 / 255, 0 / 255],
-	purple: [128 / 255, 0 / 255, 128 / 255],
-	fuchsia: [255 / 255, 0 / 255, 255 / 255],
-	green: [0 / 255, 128 / 255, 0 / 255],
-	lime: [0 / 255, 255 / 255, 0 / 255],
-	olive: [128 / 255, 128 / 255, 0 / 255],
-	yellow: [255 / 255, 255 / 255, 0 / 255],
-	navy: [0 / 255, 0 / 255, 128 / 255],
-	blue: [0 / 255, 0 / 255, 255 / 255],
-	teal: [0 / 255, 128 / 255, 128 / 255],
-	aqua: [0 / 255, 255 / 255, 255 / 255],
-	// extended color keywords
-	aliceblue: [240 / 255, 248 / 255, 255 / 255],
-	antiquewhite: [250 / 255, 235 / 255, 215 / 255],
-	// 'aqua': [ 0 / 255, 255 / 255, 255 / 255 ],
-	aquamarine: [127 / 255, 255 / 255, 212 / 255],
-	azure: [240 / 255, 255 / 255, 255 / 255],
-	beige: [245 / 255, 245 / 255, 220 / 255],
-	bisque: [255 / 255, 228 / 255, 196 / 255],
-	// 'black': [ 0 / 255, 0 / 255, 0 / 255 ],
-	blanchedalmond: [255 / 255, 235 / 255, 205 / 255],
-	// 'blue': [ 0 / 255, 0 / 255, 255 / 255 ],
-	blueviolet: [138 / 255, 43 / 255, 226 / 255],
-	brown: [165 / 255, 42 / 255, 42 / 255],
-	burlywood: [222 / 255, 184 / 255, 135 / 255],
-	cadetblue: [95 / 255, 158 / 255, 160 / 255],
-	chartreuse: [127 / 255, 255 / 255, 0 / 255],
-	chocolate: [210 / 255, 105 / 255, 30 / 255],
-	coral: [255 / 255, 127 / 255, 80 / 255],
-	cornflowerblue: [100 / 255, 149 / 255, 237 / 255],
-	cornsilk: [255 / 255, 248 / 255, 220 / 255],
-	crimson: [220 / 255, 20 / 255, 60 / 255],
-	cyan: [0 / 255, 255 / 255, 255 / 255],
-	darkblue: [0 / 255, 0 / 255, 139 / 255],
-	darkcyan: [0 / 255, 139 / 255, 139 / 255],
-	darkgoldenrod: [184 / 255, 134 / 255, 11 / 255],
-	darkgray: [169 / 255, 169 / 255, 169 / 255],
-	darkgreen: [0 / 255, 100 / 255, 0 / 255],
-	darkgrey: [169 / 255, 169 / 255, 169 / 255],
-	darkkhaki: [189 / 255, 183 / 255, 107 / 255],
-	darkmagenta: [139 / 255, 0 / 255, 139 / 255],
-	darkolivegreen: [85 / 255, 107 / 255, 47 / 255],
-	darkorange: [255 / 255, 140 / 255, 0 / 255],
-	darkorchid: [153 / 255, 50 / 255, 204 / 255],
-	darkred: [139 / 255, 0 / 255, 0 / 255],
-	darksalmon: [233 / 255, 150 / 255, 122 / 255],
-	darkseagreen: [143 / 255, 188 / 255, 143 / 255],
-	darkslateblue: [72 / 255, 61 / 255, 139 / 255],
-	darkslategray: [47 / 255, 79 / 255, 79 / 255],
-	darkslategrey: [47 / 255, 79 / 255, 79 / 255],
-	darkturquoise: [0 / 255, 206 / 255, 209 / 255],
-	darkviolet: [148 / 255, 0 / 255, 211 / 255],
-	deeppink: [255 / 255, 20 / 255, 147 / 255],
-	deepskyblue: [0 / 255, 191 / 255, 255 / 255],
-	dimgray: [105 / 255, 105 / 255, 105 / 255],
-	dimgrey: [105 / 255, 105 / 255, 105 / 255],
-	dodgerblue: [30 / 255, 144 / 255, 255 / 255],
-	firebrick: [178 / 255, 34 / 255, 34 / 255],
-	floralwhite: [255 / 255, 250 / 255, 240 / 255],
-	forestgreen: [34 / 255, 139 / 255, 34 / 255],
-	// 'fuchsia': [ 255 / 255, 0 / 255, 255 / 255 ],
-	gainsboro: [220 / 255, 220 / 255, 220 / 255],
-	ghostwhite: [248 / 255, 248 / 255, 255 / 255],
-	gold: [255 / 255, 215 / 255, 0 / 255],
-	goldenrod: [218 / 255, 165 / 255, 32 / 255],
-	// 'gray': [ 128 / 255, 128 / 255, 128 / 255 ],
-	// 'green': [ 0 / 255, 128 / 255, 0 / 255 ],
-	greenyellow: [173 / 255, 255 / 255, 47 / 255],
-	grey: [128 / 255, 128 / 255, 128 / 255],
-	honeydew: [240 / 255, 255 / 255, 240 / 255],
-	hotpink: [255 / 255, 105 / 255, 180 / 255],
-	indianred: [205 / 255, 92 / 255, 92 / 255],
-	indigo: [75 / 255, 0 / 255, 130 / 255],
-	ivory: [255 / 255, 255 / 255, 240 / 255],
-	khaki: [240 / 255, 230 / 255, 140 / 255],
-	lavender: [230 / 255, 230 / 255, 250 / 255],
-	lavenderblush: [255 / 255, 240 / 255, 245 / 255],
-	lawngreen: [124 / 255, 252 / 255, 0 / 255],
-	lemonchiffon: [255 / 255, 250 / 255, 205 / 255],
-	lightblue: [173 / 255, 216 / 255, 230 / 255],
-	lightcoral: [240 / 255, 128 / 255, 128 / 255],
-	lightcyan: [224 / 255, 255 / 255, 255 / 255],
-	lightgoldenrodyellow: [250 / 255, 250 / 255, 210 / 255],
-	lightgray: [211 / 255, 211 / 255, 211 / 255],
-	lightgreen: [144 / 255, 238 / 255, 144 / 255],
-	lightgrey: [211 / 255, 211 / 255, 211 / 255],
-	lightpink: [255 / 255, 182 / 255, 193 / 255],
-	lightsalmon: [255 / 255, 160 / 255, 122 / 255],
-	lightseagreen: [32 / 255, 178 / 255, 170 / 255],
-	lightskyblue: [135 / 255, 206 / 255, 250 / 255],
-	lightslategray: [119 / 255, 136 / 255, 153 / 255],
-	lightslategrey: [119 / 255, 136 / 255, 153 / 255],
-	lightsteelblue: [176 / 255, 196 / 255, 222 / 255],
-	lightyellow: [255 / 255, 255 / 255, 224 / 255],
-	// 'lime': [ 0 / 255, 255 / 255, 0 / 255 ],
-	limegreen: [50 / 255, 205 / 255, 50 / 255],
-	linen: [250 / 255, 240 / 255, 230 / 255],
-	magenta: [255 / 255, 0 / 255, 255 / 255],
-	// 'maroon': [ 128 / 255, 0 / 255, 0 / 255 ],
-	mediumaquamarine: [102 / 255, 205 / 255, 170 / 255],
-	mediumblue: [0 / 255, 0 / 255, 205 / 255],
-	mediumorchid: [186 / 255, 85 / 255, 211 / 255],
-	mediumpurple: [147 / 255, 112 / 255, 219 / 255],
-	mediumseagreen: [60 / 255, 179 / 255, 113 / 255],
-	mediumslateblue: [123 / 255, 104 / 255, 238 / 255],
-	mediumspringgreen: [0 / 255, 250 / 255, 154 / 255],
-	mediumturquoise: [72 / 255, 209 / 255, 204 / 255],
-	mediumvioletred: [199 / 255, 21 / 255, 133 / 255],
-	midnightblue: [25 / 255, 25 / 255, 112 / 255],
-	mintcream: [245 / 255, 255 / 255, 250 / 255],
-	mistyrose: [255 / 255, 228 / 255, 225 / 255],
-	moccasin: [255 / 255, 228 / 255, 181 / 255],
-	navajowhite: [255 / 255, 222 / 255, 173 / 255],
-	// 'navy': [ 0 / 255, 0 / 255, 128 / 255 ],
-	oldlace: [253 / 255, 245 / 255, 230 / 255],
-	// 'olive': [ 128 / 255, 128 / 255, 0 / 255 ],
-	olivedrab: [107 / 255, 142 / 255, 35 / 255],
-	orange: [255 / 255, 165 / 255, 0 / 255],
-	orangered: [255 / 255, 69 / 255, 0 / 255],
-	orchid: [218 / 255, 112 / 255, 214 / 255],
-	palegoldenrod: [238 / 255, 232 / 255, 170 / 255],
-	palegreen: [152 / 255, 251 / 255, 152 / 255],
-	paleturquoise: [175 / 255, 238 / 255, 238 / 255],
-	palevioletred: [219 / 255, 112 / 255, 147 / 255],
-	papayawhip: [255 / 255, 239 / 255, 213 / 255],
-	peachpuff: [255 / 255, 218 / 255, 185 / 255],
-	peru: [205 / 255, 133 / 255, 63 / 255],
-	pink: [255 / 255, 192 / 255, 203 / 255],
-	plum: [221 / 255, 160 / 255, 221 / 255],
-	powderblue: [176 / 255, 224 / 255, 230 / 255],
-	// 'purple': [ 128 / 255, 0 / 255, 128 / 255 ],
-	// 'red': [ 255 / 255, 0 / 255, 0 / 255 ],
-	rosybrown: [188 / 255, 143 / 255, 143 / 255],
-	royalblue: [65 / 255, 105 / 255, 225 / 255],
-	saddlebrown: [139 / 255, 69 / 255, 19 / 255],
-	salmon: [250 / 255, 128 / 255, 114 / 255],
-	sandybrown: [244 / 255, 164 / 255, 96 / 255],
-	seagreen: [46 / 255, 139 / 255, 87 / 255],
-	seashell: [255 / 255, 245 / 255, 238 / 255],
-	sienna: [160 / 255, 82 / 255, 45 / 255],
-	// 'silver': [ 192 / 255, 192 / 255, 192 / 255 ],
-	skyblue: [135 / 255, 206 / 255, 235 / 255],
-	slateblue: [106 / 255, 90 / 255, 205 / 255],
-	slategray: [112 / 255, 128 / 255, 144 / 255],
-	slategrey: [112 / 255, 128 / 255, 144 / 255],
-	snow: [255 / 255, 250 / 255, 250 / 255],
-	springgreen: [0 / 255, 255 / 255, 127 / 255],
-	steelblue: [70 / 255, 130 / 255, 180 / 255],
-	tan: [210 / 255, 180 / 255, 140 / 255],
-	// 'teal': [ 0 / 255, 128 / 255, 128 / 255 ],
-	thistle: [216 / 255, 191 / 255, 216 / 255],
-	tomato: [255 / 255, 99 / 255, 71 / 255],
-	turquoise: [64 / 255, 224 / 255, 208 / 255],
-	violet: [238 / 255, 130 / 255, 238 / 255],
-	wheat: [245 / 255, 222 / 255, 179 / 255],
-	// 'white': [ 255 / 255, 255 / 255, 255 / 255 ],
-	whitesmoke: [245 / 255, 245 / 255, 245 / 255],
-	// 'yellow': [ 255 / 255, 255 / 255, 0 / 255 ],
-	yellowgreen: [154 / 255, 205 / 255, 50 / 255],
-};
+	black: createRGB(0, 0, 0),
+	silver: createRGB(192, 192, 192),
+	gray: createRGB(128, 128, 128),
+	white: createRGB(255, 255, 255),
+	maroon: createRGB(128, 0, 0),
+	red: createRGB(255, 0, 0),
+	purple: createRGB(128, 0, 128),
+	fuchsia: createRGB(255, 0, 255),
+	green: createRGB(0, 128, 0),
+	lime: createRGB(0, 255, 0),
+	olive: createRGB(128, 128, 0),
+	yellow: createRGB(255, 255, 0),
+	navy: createRGB(0, 0, 128),
+	blue: createRGB(0, 0, 255),
+	teal: createRGB(0, 128, 128),
+	aqua: createRGB(0, 255, 255),
+	aliceblue: createRGB(240, 248, 255),
+	antiquewhite: createRGB(250, 235, 215),
 
-// export const black: RGB;
-// export const silver: RGB;
-// export const gray: RGB;
-// export const white: RGB;
-// export const maroon: RGB;
-// export const red: RGB;
-// export const purple: RGB;
-// export const fuchsia: RGB;
-// export const green: RGB;
-// export const lime: RGB;
-// export const olive: RGB;
-// export const yellow: RGB;
-// export const navy: RGB;
-// export const blue: RGB;
-// export const teal: RGB;
-// export const aqua: RGB;
-// export const aliceblue: RGB;
-// export const antiquewhite: RGB;
-// export const aquamarine: RGB;
-// export const azure: RGB;
-// export const beige: RGB;
-// export const bisque: RGB;
-// export const blanchedalmond: RGB;
-// export const blueviolet: RGB;
-// export const brown: RGB;
-// export const burlywood: RGB;
-// export const cadetblue: RGB;
-// export const chartreuse: RGB;
-// export const chocolate: RGB;
-// export const coral: RGB;
-// export const cornflowerblue: RGB;
-// export const cornsilk: RGB;
-// export const crimson: RGB;
-// export const cyan: RGB;
-// export const darkblue: RGB;
-// export const darkcyan: RGB;
-// export const darkgoldenrod: RGB;
-// export const darkgray: RGB;
-// export const darkgreen: RGB;
-// export const darkgrey: RGB;
-// export const darkkhaki: RGB;
-// export const darkmagenta: RGB;
-// export const darkolivegreen: RGB;
-// export const darkorange: RGB;
-// export const darkorchid: RGB;
-// export const darkred: RGB;
-// export const darksalmon: RGB;
-// export const darkseagreen: RGB;
-// export const darkslateblue: RGB;
-// export const darkslategray: RGB;
-// export const darkslategrey: RGB;
-// export const darkturquoise: RGB;
-// export const darkviolet: RGB;
-// export const deeppink: RGB;
-// export const deepskyblue: RGB;
-// export const dimgray: RGB;
-// export const dimgrey: RGB;
-// export const dodgerblue: RGB;
-// export const firebrick: RGB;
-// export const floralwhite: RGB;
-// export const forestgreen: RGB;
-// export const gainsboro: RGB;
-// export const ghostwhite: RGB;
-// export const gold: RGB;
-// export const goldenrod: RGB;
-// export const greenyellow: RGB;
-// export const grey: RGB;
-// export const honeydew: RGB;
-// export const hotpink: RGB;
-// export const indianred: RGB;
-// export const indigo: RGB;
-// export const ivory: RGB;
-// export const khaki: RGB;
-// export const lavender: RGB;
-// export const lavenderblush: RGB;
-// export const lawngreen: RGB;
-// export const lemonchiffon: RGB;
-// export const lightblue: RGB;
-// export const lightcoral: RGB;
-// export const lightcyan: RGB;
-// export const lightgoldenrodyellow: RGB;
-// export const lightgray: RGB;
-// export const lightgreen: RGB;
-// export const lightgrey: RGB;
-// export const lightpink: RGB;
-// export const lightsalmon: RGB;
-// export const lightseagreen: RGB;
-// export const lightskyblue: RGB;
-// export const lightslategray: RGB;
-// export const lightslategrey: RGB;
-// export const lightsteelblue: RGB;
-// export const lightyellow: RGB;
-// export const limegreen: RGB;
-// export const linen: RGB;
-// export const magenta: RGB;
-// export const mediumaquamarine: RGB;
-// export const mediumblue: RGB;
-// export const mediumorchid: RGB;
-// export const mediumpurple: RGB;
-// export const mediumseagreen: RGB;
-// export const mediumslateblue: RGB;
-// export const mediumspringgreen: RGB;
-// export const mediumturquoise: RGB;
-// export const mediumvioletred: RGB;
-// export const midnightblue: RGB;
-// export const mintcream: RGB;
-// export const mistyrose: RGB;
-// export const moccasin: RGB;
-// export const navajowhite: RGB;
-// export const oldlace: RGB;
-// export const olivedrab: RGB;
-// export const orange: RGB;
-// export const orangered: RGB;
-// export const orchid: RGB;
-// export const palegoldenrod: RGB;
-// export const palegreen: RGB;
-// export const paleturquoise: RGB;
-// export const palevioletred: RGB;
-// export const papayawhip: RGB;
-// export const peachpuff: RGB;
-// export const peru: RGB;
-// export const pink: RGB;
-// export const plum: RGB;
-// export const powderblue: RGB;
-// export const rosybrown: RGB;
-// export const royalblue: RGB;
-// export const saddlebrown: RGB;
-// export const salmon: RGB;
-// export const sandybrown: RGB;
-// export const seagreen: RGB;
-// export const seashell: RGB;
-// export const sienna: RGB;
-// export const skyblue: RGB;
-// export const slateblue: RGB;
-// export const slategray: RGB;
-// export const slategrey: RGB;
-// export const snow: RGB;
-// export const springgreen: RGB;
-// export const steelblue: RGB;
-// export const tan: RGB;
-// export const thistle: RGB;
-// export const tomato: RGB;
-// export const turquoise: RGB;
-// export const violet: RGB;
-// export const wheat: RGB;
-// export const whitesmoke: RGB;
-// export const yellowgreen: RGB;
+	aquamarine: createRGB(127, 255, 212),
+	azure: createRGB(240, 255, 255),
+	beige: createRGB(245, 245, 220),
+	bisque: createRGB(255, 228, 196),
+	blanchedalmond: createRGB(255, 235, 205),
+
+	blueviolet: createRGB(138, 43, 226),
+	brown: createRGB(165, 42, 42),
+	burlywood: createRGB(222, 184, 135),
+	cadetblue: createRGB(95, 158, 160),
+	chartreuse: createRGB(127, 255, 0),
+	chocolate: createRGB(210, 105, 30),
+	coral: createRGB(255, 127, 80),
+	cornflowerblue: createRGB(100, 149, 237),
+	cornsilk: createRGB(255, 248, 220),
+	crimson: createRGB(220, 20, 60),
+	cyan: createRGB(0, 255, 255),
+	darkblue: createRGB(0, 0, 139),
+	darkcyan: createRGB(0, 139, 139),
+	darkgoldenrod: createRGB(184, 134, 11),
+	darkgray: createRGB(169, 169, 169),
+	darkgreen: createRGB(0, 100, 0),
+	darkgrey: createRGB(169, 169, 169),
+	darkkhaki: createRGB(189, 183, 107),
+	darkmagenta: createRGB(139, 0, 139),
+	darkolivegreen: createRGB(85, 107, 47),
+	darkorange: createRGB(255, 140, 0),
+	darkorchid: createRGB(153, 50, 204),
+	darkred: createRGB(139, 0, 0),
+	darksalmon: createRGB(233, 150, 122),
+	darkseagreen: createRGB(143, 188, 143),
+	darkslateblue: createRGB(72, 61, 139),
+	darkslategray: createRGB(47, 79, 79),
+	darkslategrey: createRGB(47, 79, 79),
+	darkturquoise: createRGB(0, 206, 209),
+	darkviolet: createRGB(148, 0, 211),
+	deeppink: createRGB(255, 20, 147),
+	deepskyblue: createRGB(0, 191, 255),
+	dimgray: createRGB(105, 105, 105),
+	dimgrey: createRGB(105, 105, 105),
+	dodgerblue: createRGB(30, 144, 255),
+	firebrick: createRGB(178, 34, 34),
+	floralwhite: createRGB(255, 250, 240),
+	forestgreen: createRGB(34, 139, 34),
+	gainsboro: createRGB(220, 220, 220),
+	ghostwhite: createRGB(248, 248, 255),
+	gold: createRGB(255, 215, 0),
+	goldenrod: createRGB(218, 165, 32),
+	greenyellow: createRGB(173, 255, 47),
+	grey: createRGB(128, 128, 128),
+	honeydew: createRGB(240, 255, 240),
+	hotpink: createRGB(255, 105, 180),
+	indianred: createRGB(205, 92, 92),
+	indigo: createRGB(75, 0, 130),
+	ivory: createRGB(255, 255, 240),
+	khaki: createRGB(240, 230, 140),
+	lavender: createRGB(230, 230, 250),
+	lavenderblush: createRGB(255, 240, 245),
+	lawngreen: createRGB(124, 252, 0),
+	lemonchiffon: createRGB(255, 250, 205),
+	lightblue: createRGB(173, 216, 230),
+	lightcoral: createRGB(240, 128, 128),
+	lightcyan: createRGB(224, 255, 255),
+	lightgoldenrodyellow: createRGB(250, 250, 210),
+	lightgray: createRGB(211, 211, 211),
+	lightgreen: createRGB(144, 238, 144),
+	lightgrey: createRGB(211, 211, 211),
+	lightpink: createRGB(255, 182, 193),
+	lightsalmon: createRGB(255, 160, 122),
+	lightseagreen: createRGB(32, 178, 170),
+	lightskyblue: createRGB(135, 206, 250),
+	lightslategray: createRGB(119, 136, 153),
+	lightslategrey: createRGB(119, 136, 153),
+	lightsteelblue: createRGB(176, 196, 222),
+	lightyellow: createRGB(255, 255, 224),
+	limegreen: createRGB(50, 205, 50),
+	linen: createRGB(250, 240, 230),
+	magenta: createRGB(255, 0, 255),
+	mediumaquamarine: createRGB(102, 205, 170),
+	mediumblue: createRGB(0, 0, 205),
+	mediumorchid: createRGB(186, 85, 211),
+	mediumpurple: createRGB(147, 112, 219),
+	mediumseagreen: createRGB(60, 179, 113),
+	mediumslateblue: createRGB(123, 104, 238),
+	mediumspringgreen: createRGB(0, 250, 154),
+	mediumturquoise: createRGB(72, 209, 204),
+	mediumvioletred: createRGB(199, 21, 133),
+	midnightblue: createRGB(25, 25, 112),
+	mintcream: createRGB(245, 255, 250),
+	mistyrose: createRGB(255, 228, 225),
+	moccasin: createRGB(255, 228, 181),
+	navajowhite: createRGB(255, 222, 173),
+	oldlace: createRGB(253, 245, 230),
+	olivedrab: createRGB(107, 142, 35),
+	orange: createRGB(255, 165, 0),
+	orangered: createRGB(255, 69, 0),
+	orchid: createRGB(218, 112, 214),
+	palegoldenrod: createRGB(238, 232, 170),
+	palegreen: createRGB(152, 251, 152),
+	paleturquoise: createRGB(175, 238, 238),
+	palevioletred: createRGB(219, 112, 147),
+	papayawhip: createRGB(255, 239, 213),
+	peachpuff: createRGB(255, 218, 185),
+	peru: createRGB(205, 133, 63),
+	pink: createRGB(255, 192, 203),
+	plum: createRGB(221, 160, 221),
+	powderblue: createRGB(176, 224, 230),
+	rosybrown: createRGB(188, 143, 143),
+	royalblue: createRGB(65, 105, 225),
+	saddlebrown: createRGB(139, 69, 19),
+	salmon: createRGB(250, 128, 114),
+	sandybrown: createRGB(244, 164, 96),
+	seagreen: createRGB(46, 139, 87),
+	seashell: createRGB(255, 245, 238),
+	sienna: createRGB(160, 82, 45),
+	skyblue: createRGB(135, 206, 235),
+	slateblue: createRGB(106, 90, 205),
+	slategray: createRGB(112, 128, 144),
+	slategrey: createRGB(112, 128, 144),
+	snow: createRGB(255, 250, 250),
+	springgreen: createRGB(0, 255, 127),
+	steelblue: createRGB(70, 130, 180),
+	tan: createRGB(210, 180, 140),
+	thistle: createRGB(216, 191, 216),
+	tomato: createRGB(255, 99, 71),
+	turquoise: createRGB(64, 224, 208),
+	violet: createRGB(238, 130, 238),
+	wheat: createRGB(245, 222, 179),
+	whitesmoke: createRGB(245, 245, 245),
+	yellowgreen: createRGB(154, 205, 50),
+};
--- renameTs.sh
+++ renameTs.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
 
+# if [ "$#" -ne 1 ]; then
+#   echo "Usage: $0 <folder>"
+#   exit 1
+# fi
+
+# folder="$1"
+
+# # Check if the folder exists
+# if [ ! -d "$folder" ]; then
+#   echo "Folder '$folder' does not exist."
+#   exit 1
+# fi
+
+# # Part 1: Rename all .js files to .ts
+# find "$folder" -type f -name "*.js" -exec sh -c '
+#   for jsfile do
+#     tsfile="${jsfile%.js}.ts"
+#     mv "$jsfile" "$tsfile"
+#     echo "Renamed: $jsfile -> $tsfile"
+#   done
+# ' sh {} +
+
+# # Part 2: Append contents of .d.ts to .ts
+# find "$folder" -type f -name "*.d.ts" -exec sh -c '
+#   for dtsfile do
+#     tsfile="${dtsfile%.d.ts}.ts"
+    
+#     # Check if the .ts file exists and append the contents of .d.ts to it
+#     if [ -f "$tsfile" ]; then
+#       cat "$dtsfile" >> "$tsfile"
+#       echo "Appended contents of $dtsfile to $tsfile"
+      
+#       # Remove the .d.ts file
+#       rm "$dtsfile"
+#       echo "Deleted: $dtsfile"
+#     fi
+#   done
+# ' sh {} +
+
+# # Part 3: Delete remaining .d.ts files
+# find "$folder" -type f -name "*.d.ts" -exec sh -c '
+#   for dtsfile do
+#     rm "$dtsfile"
+#     echo "Deleted: $dtsfile"
+#   done
+# ' sh {} +
+
+# echo "Part 1: Renamed all .js files in '$folder' to .ts"
+# echo "Part 2: Appended .d.ts contents to .ts in '$folder'"
+# echo "Part 3: Deleted remaining .d.ts files in '$folder'"
+
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 <folder>"
   exit 1
@@ -13,7 +64,7 @@ if [ ! -d "$folder" ]; then
   exit 1
 fi
 
-# Part 1: Rename all .js files to .ts
+# Rename all .js files to .ts
 find "$folder" -type f -name "*.js" -exec sh -c '
   for jsfile do
     tsfile="${jsfile%.js}.ts"
@@ -22,31 +73,4 @@ find "$folder" -type f -name "*.js" -exec sh -c '
   done
 ' sh {} +
 
-# Part 2: Append contents of .d.ts to .ts
-find "$folder" -type f -name "*.d.ts" -exec sh -c '
-  for dtsfile do
-    tsfile="${dtsfile%.d.ts}.ts"
-    
-    # Check if the .ts file exists and append the contents of .d.ts to it
-    if [ -f "$tsfile" ]; then
-      cat "$dtsfile" >> "$tsfile"
-      echo "Appended contents of $dtsfile to $tsfile"
-      
-      # Remove the .d.ts file
-      rm "$dtsfile"
-      echo "Deleted: $dtsfile"
-    fi
-  done
-' sh {} +
-
-# Part 3: Delete remaining .d.ts files
-find "$folder" -type f -name "*.d.ts" -exec sh -c '
-  for dtsfile do
-    rm "$dtsfile"
-    echo "Deleted: $dtsfile"
-  done
-' sh {} +
-
-echo "Part 1: Renamed all .js files in '$folder' to .ts"
-echo "Part 2: Appended .d.ts contents to .ts in '$folder'"
-echo "Part 3: Deleted remaining .d.ts files in '$folder'"
+echo "All .js files in '$folder' have been renamed to .ts"
